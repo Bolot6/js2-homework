c75a1685dad4081bd87fa61fd09eef0cc16427ac
bolot6 <kurmanaliev78@gmail.com>
2023-10-26 17:17:14 +0600

    first commit

---
+++ script.js
@@ -0,0 +1,83 @@
+// let str= 'today is a good day'
+// let part1 = str.slice(6);
+// let part2 = str.slice(0, 5);
+// console.log(part1 + ' ' + part2);
+
+// day today gOOd
+
+
+// let part1 = str.slice(11);
+// let part2 = str.slice(11, 16);
+// let part3 = str.slice(0, 5);
+//
+// let result = part1 + ' ' + part3 + ' ' + part2.toUpperCase(2, 4);
+//
+// console.log(result)
+
+// let a = 'hello'
+// let b = 'Ivan'
+// console.log(a[0].toUpperCase() + a.substr(1) + ' ' + b[0].toLowerCase() + b.substr(1));
+
+// let a = 'lorem ipsum dolor'
+// let b = a .length
+// длина 17
+// console.log(b)
+
+// let a = 'lorem ipsum dolor sit amet'
+// // вернуть sit lorem ipsum dolor amet
+// let part1 = a.slice(18,21)
+// let part2 = a.slice(21, 26)
+// let part3 = a.slice(0, 17)
+// console.log(part1+' '+part3+part2)
+
+// n
+
+// let a = 'boss'
+// // вернуть bsso,
+// console.log (a.slice(0, 1) + a.slice(2, 3) + a.slice(2, 3) + a.slice(1, 2));
+
+// let a = 'sunday'
+// // вернуть index d
+// значение d=3
+// console.log(a.indexOf('d'))
+
+// let a = 'hello'
+// // вернуть hekso
+// let b = a.slice(0,2)
+// let c =a.slice(4)
+// let d ='ks'
+// console.log(b+d+c)
+
+// let a = 'user'
+// let b = 'new'
+// // вернуть new us
+// console.log(b+' '+a.slice(0,2))
+
+// let a = 'monday'
+// let b = 'day'
+// // вернуть daydaymon
+// console.log(b+b+a.slice(0,3))
+
+// let a = 'HELLO'
+// // вернуть 'hElLo'
+// let part1 = a.slice(0, 1).toLowerCase()
+// let part2 = a.slice(1, 2)
+// let part3 = a.slice(2, 3) .toLowerCase()
+// let part4 = a.slice(3, 4)
+// let part5 = a.slice(4) .toLowerCase()
+// console.log(part1+part2+part3+part4+part5)
+
+// let a = 5
+// let b = '400'
+// let c = b.slice(0, 1)
+// let d = b.slice(1,2)
+// // вернуть '450'
+// console.log(c+a+d)
+
+let a = 'monkey'
+let b = 'banana'
+// вернуть manak
+console.log(a.slice(0, 1)+ b.slice(1, 4)+ a.slice(3,4))
+
+
+
